06f75f6b01930670edde8c217e186b2a40d889f8
grzesiek-K1 <82473315+grzesiek-K1@users.noreply.github.com>
2021-04-14 13:37:02 +0200

    Create projekt-lista.py

---
+++ projekt-lista.py
@@ -0,0 +1,8 @@
+a = int(input('Podaj liczbę: '))
+if(a>0):
+    print('Ta liczba jest dodatnia')
+elif(a==0):
+    print('Ta liczba jest zerem')
+else:
+    print('Ta liczba jest ujemna')
+print('udało sie')
